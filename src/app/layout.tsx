a9565836f806c22e6dc703f0fb723c632c6f8b0a
Charlotte-greenaway <charlottegreenaway654@gmail.com>
2024-02-23 20:35:20 +0000

    seo improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
- import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from "next";
 import "./globals.css";
 import SessionProvComponent from "./providers/SessionProvider";
 import UIProvider from "./providers/UiProvider";
@@ -7,7 +6,8 @@ import NavBar from "./components/navbar";
 import Timer from "./components/buttons/timer";
 export const metadata: Metadata = {
   title: "Dash Dish",
-  description: "The recipe book for your kitchen - find recipes based on the ingredients you have at home.",
+  description:
+    "The recipe book for your kitchen - find recipes based on the ingredients you have at home.",
 };
 
 export default function RootLayout({
@@ -17,16 +17,34 @@ export default function RootLayout({
 }>) {
   return (
     <html>
+      <head>
+        <meta
+          property="og:title"
+          content="Dash Dish - Discover Delicious Recipes"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.dashdish.co.uk" />
+        <meta
+          property="og:description"
+          content="Explore Dash Dish to find the perfect recipe for any occasion. Utilize the ingredients you already have at home to create delicious meals."
+        />
+        <meta
+          property="og:image"
+          content="https://www.dashdish.co.uk/_next/static/media/logo.c58f19bb.png"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="content-language" content="en" />
+        <meta name="robots" content="index, follow" />
+      </head>
       <body>
-      <SessionProvComponent>
-        <UIProvider>
-
+        <SessionProvComponent>
+          <UIProvider>
             <NavBar />
             {children}
-            <Timer/>
-        </UIProvider>
-      </SessionProvComponent>
+            <Timer />
+          </UIProvider>
+        </SessionProvComponent>
       </body>
-      </html>
+    </html>
   );
 }
